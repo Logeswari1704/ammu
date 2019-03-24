7fe30af1001e636dce547359d87e835040daa48a
Logeswari J <46586799+Logeswari1704@users.noreply.github.com>
2019-03-24 22:00:12 +0530

    Create hunter-set1-1.py

---
+++ hunter-set1-1.py
@@ -0,0 +1,14 @@
+# Hunter Set 1 Problem 1
+
+import collections as col
+
+n = int(input())
+nums = list(map(int, input().split()))
+c = col.Counter(nums)
+o = {k for k, v in c.items() if v > 1}
+
+if o:
+    for i in o:
+        print(*o)
+else:
+    print('unique')
